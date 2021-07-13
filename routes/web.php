a27cd5c2da30f165ed1f28f9811899cc8d5c04fa
Dac Phuong <phuonghdth2009010@gmail.com>
2021-07-13 10:25:21 +0700

    data-handle upload

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\DataHandleController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/data-handle/{id}/path',[DataHandleController::class,'handlePathVariable']);
+Route::get('/data-handle/query-string',[DataHandleController::class,'handleQueryString']);
+Route::get('/data-handle/form',[DataHandleController::class,'returnForm']);
+Route::post('/data-handle/form',[DataHandleController::class,'processForm']);
